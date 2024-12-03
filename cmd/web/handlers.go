56ba17270e30fe7f7b25fba8c18e6d3bb3b5e290
tvsanti <santithevenetvalles@gmail.com>
2024-12-03 20:55:31 +0100

    feat: add more views

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -60,7 +60,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", snippet)
+	data := templateData{
+		Snippet: snippet,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
 	}
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "snpippetbox.santiago.net/internal/models"
+
+type templateData struct {
+	Snippet models.Snippet
+}
